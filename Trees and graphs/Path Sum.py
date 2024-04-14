4ab4d5a4e3b7c5c84b41f98d17ff889daf7bacb5
Seulgie <gkstmfrl917@naver.com>
2024-04-14 18:05:23 +0900

    Create Path Sum.py

---
+++ Trees and graphs/Path Sum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curr):
+            if not node:
+                return False
+
+            # if both children are null, then the node is a leaf
+            if node.left == None and node.right == None:
+                return (curr + node.val) == targetSum
+
+            curr += node.val
+            left = dfs(node.left, curr)
+            right = dfs(node.right, curr)
+            return left or right
+
+        return dfs(root, 0)
