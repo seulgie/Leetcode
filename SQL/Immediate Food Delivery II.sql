ea596f9b795c9b15596df04b616b394065f68252
Seulgie <gkstmfrl917@naver.com>
2024-04-17 18:15:02 +0900

    Create Immediate Food Delivery II.sql

---
+++ SQL/Immediate Food Delivery II.sql
@@ -0,0 +1,9 @@
+SELECT
+    ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1, 0))*100, 2) AS immediate_percentage
+FROM 
+(
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM Delivery
+) AS t
+WHERE rn = 1
