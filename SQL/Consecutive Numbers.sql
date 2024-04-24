8e3d3df65bf3e997f57c290dac8fec54ffd7bc5e
Seulgie <gkstmfrl917@naver.com>
2024-04-24 11:09:15 +0900

    Update Consecutive Numbers.sql

--- SQL/Consecutive Numbers.sql
+++ SQL/Consecutive Numbers.sql
@@ -1,12 +1,7 @@
-WITH CTE AS 
-(
-    SELECT
-        num,
-        LEAD(num, 1) OVER() num1,
-        LEAD(num, 2) OVER() num2
-    FROM Logs
-)
-
-SELECT DISTINCT(num) AS ConsecutiveNums
-FROM CTE
-WHERE (num=num1) AND (num=num2)
+SELECT DISTINCT(l1.num) AS ConsecutiveNums
+FROM Logs as l1 
+LEFT JOIN Logs as l2
+ON l1.id = l2.id - 1
+LEFT JOIN Logs as l3
+ON l2.id = l3.id - 1
+WHERE l1.num = l2.num AND l2.num = l3.num
